92a2e98e608956d451f5e8c78697b161bbe34b12
Adina Yusuf <adinkayus97@gmail.com>
2022-07-02 17:39:44 +0600

    исправила ошибки в json

--- webapp/models.py
+++ webapp/models.py
@@ -11,7 +11,7 @@ class GuestBook(models.Model):
     text = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Текст")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     edit_time = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-    status = models.CharField( max_length=15, choices=STATUS, null=False, default=STATUS[0][0], verbose_name='Статус')
+    status = models.CharField(max_length=15, choices=STATUS, null=False, default=STATUS[0][0], verbose_name='Статус')
 
     def __str__(self):
         return f'{self.id}.{self.name}: {self.status} '
